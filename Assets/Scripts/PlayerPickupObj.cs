c0651cb0401868de9b6ae512989568f9cd8f5b9b
ICROSStheLINE <coolwillrule123@gmail.com>
2025-03-31 01:14:38 -0400

    Made the prop pen pick-up-able using leftclick! Also added a crosshair

--- Assets/Scripts/PlayerPickupObj.cs
+++ Assets/Scripts/PlayerPickupObj.cs
@@ -4,6 +4,7 @@ public class PlayerPickupObj : MonoBehaviour
 {
     LayerMask layerMask;
 	Transform playerPOV;
+	[SerializeField] GameObject penContainer;
 
 	void Start()
 	{
@@ -13,7 +14,7 @@ public class PlayerPickupObj : MonoBehaviour
 	
 	void FixedUpdate()
 	{
-		if (Input.GetKey("g"))
+		if (Input.GetKey(KeyCode.Mouse0))
 		{
 			PickUpItem();
 		}
@@ -24,6 +25,10 @@ public class PlayerPickupObj : MonoBehaviour
 		RaycastHit hit;
 		if (Physics.Raycast(playerPOV.position, playerPOV.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
 		{
+			hit.transform.SetParent(penContainer.transform);
+			hit.transform.localPosition = Vector3.zero;
+			hit.transform.localRotation = Quaternion.Euler(Vector3.zero);
+			hit.transform.GetComponent<Rigidbody>().isKinematic = true;
 			Debug.Log("Selected an object!! Yippee!! \n" + "Object name: " + hit.transform.gameObject.name + "\n" + "Object distance from player: " + hit.distance);
 		}
 	}
