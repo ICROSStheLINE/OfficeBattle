59245d9300ffdd34c7002893b3ad52cc7168d6e6
ICROSStheLINE <coolwillrule123@gmail.com>
2025-04-04 18:30:31 -0400

    Made any currently held prop droppable when mouse-right-click is pressed! Yippee! Also made it so that the player cannot pick up another object if they are already holding one.

--- Assets/Scripts/PlayerPickupObj.cs
+++ Assets/Scripts/PlayerPickupObj.cs
@@ -5,6 +5,7 @@ public class PlayerPickupObj : MonoBehaviour
     LayerMask layerMask;
 	Transform playerPOV;
 	[SerializeField] GameObject penContainer;
+	GameObject currentlyHeldObject = null;
 
 	void Start()
 	{
@@ -21,7 +22,13 @@ public class PlayerPickupObj : MonoBehaviour
 	{
 		if (Input.GetKey(KeyCode.Mouse0))
 		{
-			PickUpItem();
+			if (currentlyHeldObject == null)
+				PickUpItem();
+		}
+		if (Input.GetKey(KeyCode.Mouse1))
+		{
+			if (currentlyHeldObject != null)
+				DropItem();
 		}
 	}
 
@@ -30,6 +37,7 @@ public class PlayerPickupObj : MonoBehaviour
 		RaycastHit hit;
 		if (Physics.Raycast(playerPOV.position, playerPOV.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
 		{
+			currentlyHeldObject = hit.transform.gameObject;
 			hit.transform.SetParent(penContainer.transform);
 			hit.transform.localPosition = Vector3.zero;
 			hit.transform.localRotation = Quaternion.Euler(Vector3.zero);
@@ -37,5 +45,13 @@ public class PlayerPickupObj : MonoBehaviour
 			hit.transform.GetComponent<BoxCollider>().isTrigger = true;
 			Debug.Log("Selected an object!! Yippee!! \n" + "Object name: " + hit.transform.gameObject.name + "\n" + "Object distance from player: " + hit.distance);
 		}
+	}
+	
+	void DropItem()
+	{
+		currentlyHeldObject.transform.SetParent(null);
+		currentlyHeldObject.transform.GetComponent<Rigidbody>().isKinematic = false;
+		currentlyHeldObject.transform.GetComponent<BoxCollider>().isTrigger = false;
+		currentlyHeldObject = null;
 	}
 }
