19b6e978b8f8b841e4466d2c717bd0d06b46b102
ICROSStheLINE <coolwillrule123@gmail.com>
2025-04-27 01:05:57 -0400

    Working towards making procedural walking motion. Added a script called StepManager that was initially intended to micromanage each footstep, but that may not be necessary for now as I continue to test possible solutions. For now, I added some documentation in that script explaining my thought process around continuing development on existing scripts. Looking good so far!

--- Assets/Scripts/IKWalkingAnim.cs
+++ Assets/Scripts/IKWalkingAnim.cs
@@ -6,26 +6,38 @@ public class IKWalkingAnim : MonoBehaviour
 {
 	LayerMask layerMask;
 	GameObject rayPositionGameObject;
+	GameObject standingPositionGameObject;
 	Vector3 currentPosition;
+
+	float stepDistanceThreshhold = 1f;
 
     void Start()
     {
 		rayPositionGameObject = transform.parent.gameObject.transform.Find("rayPosition").gameObject;
-		
+		standingPositionGameObject = transform.parent.gameObject.transform.Find("StandingPosition").gameObject;
+
         layerMask = LayerMask.GetMask("Terrain");
+
+		Vector3 standingPosition = standingPositionGameObject.transform.position;
+		PlaceFootOnRaycast(standingPosition + Vector3.up * 2, Vector3.down, 0);
     }
 
     void Update()
     {
 		Vector3 rayPosition = rayPositionGameObject.transform.position;
 		transform.position = currentPosition;
-		Ray ray = new Ray(rayPosition + Vector3.up * 2, Vector3.down);
+		PlaceFootOnRaycast(rayPosition + Vector3.up * 2, Vector3.down, stepDistanceThreshhold);
+    }
+
+	void PlaceFootOnRaycast(Vector3 rayStartPos, Vector3 normalizedDirection, float stepDistanceThreshhold_)
+	{
+		Ray ray = new Ray(rayStartPos, normalizedDirection);
 		if (Physics.Raycast(ray, out RaycastHit hit, 15, layerMask))
 		{
-			if (Vector3.Distance(currentPosition, hit.point) > 1f)
+			if (Vector3.Distance(currentPosition, hit.point) > stepDistanceThreshhold_)
 			{
 				currentPosition = hit.point + new Vector3(0,0.2f,0);
 			}
 		}
-    }
+	}
 }
---
+++ Assets/Scripts/StepManager.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+
+public class StepManager : MonoBehaviour
+{
+    GameObject steppingTarget;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Before running, have the steppingposition/rayposition gameObject target
+        // a position AHEAD of standing position, but not far enough ahead to exceed
+        // the stepDistanceThreshhold defined in IKWalkingAnim.
+
+        // On runtime, make the player stand on the StandingPosition gameobject,
+        // then have the player feet teleport to their future positions as already
+        // coded. 
+
+        // COMPLETED <--
+
+        // ==================================
+
+        // Once that has been developed properly move on to stage two: Making the feet
+        // return to their standing position when not moving.
+
+        // This will be difficult to do at this time because testing the stepping code
+        // has only been done through manually moving the player on runtime through
+        // the scene view.
+
+        // To go around this hurdle for testing, you can make a serialized boolean
+        // called "isStanding" that you can set to true whenever you want to simulate
+        // what would happen if the player was not trying to move.
+        
+        // ==================================
+        
+        // Once that is complete, begin stage 3: Making the stepping motion
+        // multi-directional.
+
+        // This would probably require a rewrite of how the raycasting is called.
+        // Instead of casting rays at "rayPosition" that go straight down, maybe cast
+        // some rays that start at the player's waist that don't point straight down,
+        // but instead point DIAGONALLY down. Ideally, have it point in around the
+        // same positions as where the rayPosition gameobject is currently set to.
+
+        // Maybe have some kind of signifier showing where the raycasts are pointing to
+        // in the form of a test gameobject that constantly moves to the position of
+        // where the raycasts are hitting, and make the angle of the raycasts a 
+        // serialized variable that you can move around on runtime.
+
+        // When that's done, you can freely rotate the raycast direction to match the
+        // direction of the player's movement.
+
+        // NOTE: Maybe make the position that the raycast hit call another raycast
+        // straight down, just in case the raycast hits a WALL instead of a FLOOR.
+        // Further testing required.
+
+        // Alternatively if you want to be cringe, you DON'T need to rewrite the
+        // raycasting system. You can just choose to have an empty gameobject
+        // that acts as the rayPosition's parent object and you can set its
+        // position to be the player's centre position (so that would be directly
+        // between the player's feet I guess) and just rotate that around to match
+        // the direction of the player's movement.
+
+        // Pros to rewriting raycasting system:
+        // - If an object (like a wall) is super close to the player their feet
+        // they can position their feet to not go through the wall.
+        // - Technical details are subconsciously noticeable in-game by the player.
+
+        // Cons to rewriting raycasting system:
+        // - Takes a lot of time and headache
+        // - Might make you slightly suicidal
+        
+        // ==================================
+
+        // Have the stepping animation triggered by in-game player-inputted movement
+        // as opposed to a serialized variable that dictates when the player is walking.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
