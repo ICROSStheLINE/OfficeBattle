8d726219a67623a373308e914eab65958d5a73cc
ICROSStheLINE <coolwillrule123@gmail.com>
2025-09-03 12:17:29 -0400

    - Completed a basic version of the running punch animation! Looks good so far

---
+++ Assets/Scripts/Punching.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Punching : MonoBehaviour
+{
+    /*static readonly float runPunchAnimationDurationSpeedMultiplier = 1f;
+	static readonly float runPunchAnimationDuration = 1.083f / runPunchAnimationDurationSpeedMultiplier;
+	static readonly float runPunchAnimationFrames = 26f;
+	static readonly float itemGrabbedFrame = 8f;
+	static readonly float secondsUntilItemGrabbed = (itemGrabbedFrame / animationFrames) * animationDuration;
+	static readonly float secondsBetweenItemGrabAndAnimationEnd = animationDuration - secondsUntilItemGrabbed;*/
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
