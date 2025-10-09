233f57908fe61a0425e15a53276a6f71552f11cf
ICROSStheLINE <coolwillrule123@gmail.com>
2025-10-09 16:10:05 -0400

    - Worked towards adding 1-step punch
    - Added the 1-step animation in-game
    
    - Need to make it triggerable in punching.cs
    
    - Fixed a bug that made punching the dummy crash the game. It was caused by the dummy not having the blocking script.

--- Assets/Scripts/Punching.cs
+++ Assets/Scripts/Punching.cs
@@ -19,22 +19,25 @@ public class Punching : NetworkBehaviour
     Vector3 constantPlayerMovement = default(Vector3);
     float constantPlayerSpeed = default(float);
     Vector3 playerMovementTarget = default(Vector3);
-
-    // -- 2 Step Running Punch
     bool isMidRunningPunch = false;
     float punchGap = 2;
+
+    // -- 1 Step Running Punch
+    Vector2 oneStepRunningPunchTriggerRange = new Vector2(4f, 12f);
+
+    // -- 2 Step Running Punch
     Vector2 twoStepRunningPunchTriggerRange = new Vector2(8f, 12f);
 
     // -- 2 Step Running Punch Animation Variables
-    static readonly float runPunchAnimationDurationSpeedMultiplier = 1f;
-    static readonly float runPunchAnimationDuration = 1.083f / runPunchAnimationDurationSpeedMultiplier;
-    static readonly float runPunchAnimationFrames = 26f;
-    static readonly float runPunchDamageActivationFrame = 18f;
-    static readonly float runPunchDamageDeactivationFrame = 20f;
-    static readonly float secondsUntilRunPunchDamageActivation = (runPunchDamageActivationFrame / runPunchAnimationFrames) * runPunchAnimationDuration;
-    static readonly float secondsUntilRunPunchDamageDeactivation = (runPunchDamageDeactivationFrame / runPunchAnimationFrames) * runPunchAnimationDuration;
-    static readonly float secondsBetweenRunPunchDamageActivationAndDeactivation = secondsUntilRunPunchDamageDeactivation - secondsUntilRunPunchDamageActivation;
-    static readonly float secondsBetweenRunPunchDamageDeactivationAndEnd = runPunchAnimationDuration - secondsUntilRunPunchDamageDeactivation;
+    static readonly float twoStepRunPunchAnimationDurationSpeedMultiplier = 1f;
+    static readonly float twoStepRunPunchAnimationDuration = 1.083f / twoStepRunPunchAnimationDurationSpeedMultiplier;
+    static readonly float twoStepRunPunchAnimationFrames = 26f;
+    static readonly float twoStepRunPunchDamageActivationFrame = 18f;
+    static readonly float twoStepRunPunchDamageDeactivationFrame = 20f;
+    static readonly float secondsUntiltwoStepRunPunchDamageActivation = (twoStepRunPunchDamageActivationFrame / twoStepRunPunchAnimationFrames) * twoStepRunPunchAnimationDuration;
+    static readonly float secondsUntiltwoStepRunPunchDamageDeactivation = (twoStepRunPunchDamageDeactivationFrame / twoStepRunPunchAnimationFrames) * twoStepRunPunchAnimationDuration;
+    static readonly float secondsBetweentwoStepRunPunchDamageActivationAndDeactivation = secondsUntiltwoStepRunPunchDamageDeactivation - secondsUntiltwoStepRunPunchDamageActivation;
+    static readonly float secondsBetweentwoStepRunPunchDamageDeactivationAndEnd = twoStepRunPunchAnimationDuration - secondsUntiltwoStepRunPunchDamageDeactivation;
 
     void Start()
     {
@@ -73,17 +76,21 @@ public class Punching : NetworkBehaviour
             {
                 if (playerStats.isRunning && hit.distance > twoStepRunningPunchTriggerRange.x && hit.distance < twoStepRunningPunchTriggerRange.y)
                 {
-                    StartCoroutine(RunningPunch(hit.transform.gameObject));
+                    StartCoroutine(Punch("twoStepPunching", hit.transform.gameObject, secondsUntiltwoStepRunPunchDamageActivation, secondsBetweentwoStepRunPunchDamageActivationAndDeactivation, secondsBetweentwoStepRunPunchDamageDeactivationAndEnd));
+                }
+                if (playerStats.isRunning && hit.distance > oneStepRunningPunchTriggerRange.x && hit.distance < oneStepRunningPunchTriggerRange.y)
+                {
+                    // one step punch
                 }
             }
         }
     }
 
-    IEnumerator RunningPunch(GameObject punchTarget)
+    IEnumerator Punch(string punchType, GameObject punchTarget, float secondsUntilPunchDamageActivation, float secondsBetweenPunchDamageActivationAndDeactivation, float secondsBetweenPunchDamageDeactivationAndEnd)
     {
         // -- Player cannot move or turn the camera while punching
         isMidRunningPunch = true;
-        anim.SetBool("isPunching", true);
+        anim.SetBool(punchType, true);
         playerStats.canMove = false;
         playerStats.canTurn = false;
 
@@ -95,9 +102,9 @@ public class Punching : NetworkBehaviour
         constantPlayerMovement = default(Vector3);
         constantPlayerSpeed = default(float);
 
-        yield return new WaitForSeconds(secondsUntilRunPunchDamageActivation);
+        yield return new WaitForSeconds(secondsUntilPunchDamageActivation);
         handDamageActive = true; // -- Turn on the damage hitboxes on the punch
-        yield return new WaitForSeconds(secondsBetweenRunPunchDamageActivationAndDeactivation);
+        yield return new WaitForSeconds(secondsBetweenPunchDamageActivationAndDeactivation);
 
         // -- Player will no longer run towards that point where the opponent was.
         // -- Instead, player will blindly move forward, but at a snails pace
@@ -107,7 +114,7 @@ public class Punching : NetworkBehaviour
         constantPlayerSpeed = playerMovement.forwardMoveSpeed / 4f;
 
         handDamageActive = false; // -- Turn off the damage hitboxes on the punch
-        yield return new WaitForSeconds(secondsBetweenRunPunchDamageDeactivationAndEnd);
+        yield return new WaitForSeconds(secondsBetweenPunchDamageDeactivationAndEnd);
 
         // -- No more custom movement
         playerMovementTarget = default(Vector3);
@@ -115,7 +122,7 @@ public class Punching : NetworkBehaviour
         constantPlayerSpeed = default(float);
 
         // -- Player is re-granted ability to move and turn the camera
-        anim.SetBool("isPunching", false);
+        anim.SetBool(punchType, false);
         isMidRunningPunch = false;
         playerStats.canMove = true;
         playerStats.canTurn = true;
@@ -127,13 +134,13 @@ public class Punching : NetworkBehaviour
         {
 			Blocking collidedObjBlockingComponent = collidedObject.transform.root.GetComponent<Blocking>();
 			// Get directions
-            Vector3 defenderForward = collidedObject.transform.root.forward;
-            Vector3 toAttacker = (gameObject.transform.position - collidedObject.transform.root.position).normalized;
+            //Vector3 defenderForward = collidedObject.transform.root.forward;
+            //Vector3 toAttacker = (gameObject.transform.position - collidedObject.transform.root.position).normalized;
 
             // Calculate angle between defender forward and attacker direction
-            float angleOffsetFromPlayer = Vector3.Angle(defenderForward, toAttacker); // 0 means target is facing the player dead-on
+            //float angleOffsetFromPlayer = Vector3.Angle(defenderForward, toAttacker); // 0 means target is facing the player dead-on
 
-			if (collidedObjBlockingComponent.isBlocking.Value && angleOffsetFromPlayer <= 30f)// If opponent was blocking
+			if (collidedObjBlockingComponent.isBlocking.Value)// If opponent was blocking // && angleOffsetFromPlayer <= 30f
 			{
 				
 				handDamageActive = false;
